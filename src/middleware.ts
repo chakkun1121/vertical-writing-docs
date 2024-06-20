53a69e86ba83e36aba69f6f3b78f15cf47dc2406
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-06-20 17:36:49 +0900

    protect page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,16 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+
+const PUBLIC_PAGES = ["", "/", "/login"];
+export default auth(req => {
+  if (
+    !req.auth &&
+    !PUBLIC_PAGES.some(page => page === new URL(req.url).pathname)
+  ) {
+    const newUrl = new URL("/login", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
